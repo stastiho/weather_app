99f192a8a14ce0ca3b6ba7223c7f7dd925ef4518
Станислав Тихомиров <s.tikhomirov@stilsoft.tech>
2024-11-09 22:22:19 +0300

    feat: add delete button for desktop

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSwipeable } from 'react-swipeable';
 
@@ -11,19 +11,31 @@ interface WeatherCardProps {
 }
 
 const WeatherCard: React.FC<WeatherCardProps> = ({ location, temperature, weatherIcon, weatherDescription, onRemove }) => {
-		const { t } = useTranslation();
+    const { t } = useTranslation();
+    const [isHovered, setIsHovered] = useState(false);
+
     const handlers = useSwipeable({
         onSwipedRight: onRemove,
     });
 
-    return (
-        <div {...handlers} className="weather-card">
-            <h2>{location}</h2>
-            <img src={weatherIcon} alt={weatherDescription} />
-						<p>{t(weatherDescription.toLowerCase().replaceAll(' ', '_'))}</p>
-            <p>Температура: {temperature}°C</p>
-        </div>
-    );
+		return (
+			<div
+					{...handlers}
+					className={`weather-card ${isHovered ? 'hovered' : ''}`}
+					onMouseEnter={() => setIsHovered(true)}
+					onMouseLeave={() => setIsHovered(false)}
+			>
+					<button className="remove-button" onClick={onRemove}>
+							Х
+					</button>
+					<div className="weather-card-content">
+							<h2>{location}</h2>
+							<img src={weatherIcon} alt={weatherDescription} />
+							<p>{t(weatherDescription?.toLowerCase().replaceAll(' ', '_'))}</p>
+							<p>Температура: {temperature}°C</p>
+					</div>
+			</div>
+	);
 };
 
 export default WeatherCard;
