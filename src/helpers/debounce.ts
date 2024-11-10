0effa95afebd1f8da02b2d6af852f1dd0921b34d
Станислав Тихомиров <s.tikhomirov@stilsoft.tech>
2024-11-10 23:11:57 +0300

    fix: debounce ts error

--- src/helpers/debounce.ts
+++ src/helpers/debounce.ts
@@ -1,5 +1,5 @@
 export const debounce = (func: (...args: any[]) => void, delay: number) => {
-	let timeoutId: NodeJS.Timeout;
+	let timeoutId: ReturnType<typeof setTimeout>;
 	return (...args: any[]) => {
 			clearTimeout(timeoutId);
 			timeoutId = setTimeout(() => func(...args), delay);
